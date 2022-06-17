8a282b0452676e5cf96ee39ef834e847db5e1416
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-17 18:02:42 +0100

    adding solution to refactor

--- cbh-take-home-project/dpk.js
+++ cbh-take-home-project/dpk.js
@@ -3,24 +3,22 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let partitionKey;
   const data = JSON.stringify(event);
 
   if (event) {
     // Ternary operator to determine if the event has a partition key
-    event = event.partitionKey? candidate = event.partitionKey: candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+    event = event.partitionKey? partitionKey = event.partitionKey: partitionKey = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  if (candidate) {
-    console.log(candidate)
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+   if (partitionKey) {
+      partitionKey= typeof partitionKey !== "string" 
+      partitionKey = JSON.stringify(partitionKey);
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    partitionKey = TRIVIAL_PARTITION_KEY;
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
   }
-  return candidate;
+  return partitionKey;
 };
