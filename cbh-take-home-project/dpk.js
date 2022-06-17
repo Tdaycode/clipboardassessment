cd3caf4e31c2e104b6920558b20f0ff6dfce2ab8
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-17 18:43:07 +0100

    adding refactor

--- cbh-take-home-project/dpk.js
+++ cbh-take-home-project/dpk.js
@@ -5,17 +5,20 @@ exports.deterministicPartitionKey = (input) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let partitionResult;
 
+  // Checking if partition key already exist
+  if(input.partitionKey) partitionResult = input.partitionKey;
+
   if (input) {
-    // Generate a hash of the event data
+    /* Generate a hash of the event data */
       const data = JSON.stringify(input);
       partitionResult = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  // Checking type of partitionResult
+  /* Checking type of partitionResult */
   if (typeof partitionResult !== "string") {
     partitionResult = JSON.stringify(partitionResult);
   }
-  // Checking if partitionResult exist
+  /* Checking if partitionResult exist */
   if (!partitionResult) partitionResult = TRIVIAL_PARTITION_KEY;
 
   if (partitionResult.length > MAX_PARTITION_KEY_LENGTH) 
