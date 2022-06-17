cade1905a122957adfa0422eddc901efc1e52076
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-06-17 18:16:08 +0100

    adding refractor

--- cbh-take-home-project/dpk.js
+++ cbh-take-home-project/dpk.js
@@ -1,15 +1,15 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (data) => {
+exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (data) {
-    if (data.partitionKey) {
-      candidate = data.partitionKey;
+  if (event) {
+    if (event.partitionKey) {
+      candidate = event.partitionKey;
     } else {
-      const data = JSON.stringify(data);
+      const data = JSON.stringify(event);
       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
     }
   }
